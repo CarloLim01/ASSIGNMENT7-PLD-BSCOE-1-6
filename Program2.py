65b537724d93fc3d44399cbc7a125cc6a62925a0
CarloLim <CarloLim01>
2021-12-02 23:20:14 +0800

    Program 2 (Asking password to the user)

---
+++ Program2.py
@@ -0,0 +1,11 @@
+#Create a program that check if password is valid
+#The password is valid if all criteria are met:
+#A. Greater than 15 letters
+#B. Have at least one capital letter
+#C. Have at least one number
+#D. Have at least one special char (!@#$%^&*()_+ etc)
+#Example:
+#Input: P@ssw0rd+P@ssw0rd
+#Ouput: Valid
+
+password = input("Enter your password: ")
