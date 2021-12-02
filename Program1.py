506b36e68f296ff49bf6c64da78a7174e5b80b95
CarloLim <CarloLim01>
2021-12-02 22:52:34 +0800

    Program 1 (Asking sentence to the user)

---
+++ Program1.py
@@ -0,0 +1,10 @@
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants1 in the input 
+#Example:
+#Input: Bahala kayo dyan
+#Output:
+#Words: 3
+#Vowels: 6
+#Consonants: 8
+
+user_sentence = input("Enter a sentence: ")
