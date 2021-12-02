680a70bb68fae437a5926267cb0844af1c76b003
CarloLim <CarloLim01>
2021-12-02 23:07:34 +0800

    Program 1 (Adding number of consonants)

--- Program1.py
+++ Program1.py
@@ -19,3 +19,10 @@ def vowels_number():
         if (num == "a" or num == "A" or num == "e" or num == "E" or num == "i" or num == "I" or num == "o" or num == "O" or num == "u" or num == "U"):
             count = count + 1
     print(f"Number of Vowels: {count}")
+
+def consonants_number():
+    count = 0
+    for num in user_sentence:
+        if (num == "b" or num ==  "B" or num == "c" or num == "C" or num == "d" or num == "D" or num == "f" or num == "F" or num == "g" or num == "G" or num == "h" or num == "H" or num == "j" or num == "J" or num == "k" or num == "K" or num == "l" or num == "L" or num == "m" or num == "M" or num == "n" or num == "N" or num == "p" or num == "P" or num == "q" or num == "Q" or num == "r" or num == "R" or num == "s" or num == "S" or num == "t" or num == "T" or num == "v" or num == "V" or num == "w" or num == "W" or num == "x" or num == "X" or num == "y" or num == "Y" or num == "z" or num == "Z"):
+            count = count + 1
+    print(f"Number of Consonants: {count}")
